efd4598cda3db87c9e906405ef9e008610f6af89
Sadiya <160082120+sadiyameem@users.noreply.github.com>
2026-01-13 19:47:08 -0500

    added more prompts

--- script.js
+++ script.js
@@ -2,7 +2,20 @@ function createStory() {
     let noun = document.getElementById("noun");
     let verb = document.getElementById("verb");
     let color = document.getElementById("color");
+    let adjective = document.getElementById("adjective").value;
+    let place = document.getElementById("place").value;
+    let animal = document.getElementById("animal").value;
+    let emotion = document.getElementById("emotion").value;
+    let food = document.getElementById("food").value;
+
     let story = document.getElementById("story");
 
-    story.innerHTML = "I met a " + noun.value + " who could " + verb.value + " very fast. He wore a " + color.value + " shirt.";
+    story.innerHTML = 
+    "I met a " + noun.value + 
+    " who could " + verb.value + 
+    " very fast. He wore a " + color.value + 
+    " shirt." + " It was a " + adjective + 
+    " day when we went to the " + place + "." +
+    " Suddenly, a " + animal + " appeared and made me feel " + emotion + "." +
+    " Then we ate some " + food + " together. ";
 }
