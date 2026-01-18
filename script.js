c9b73285a6f2f981744d0b108ffcee74c365f24f
Sadiya <160082120+sadiyameem@users.noreply.github.com>
2026-01-18 10:44:23 -0500

    added more prompts and spaced out the code

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ function createStory() {
     let place = document.getElementById("place").value;
     let animal = document.getElementById("animal").value;
     let emotion = document.getElementById("emotion").value;
+    let number = document.getElementById("number").value;
     let food = document.getElementById("food").value;
+    let object = document.getElementById("object").value;
 
     let story = document.getElementById("story");
 
@@ -17,5 +19,6 @@ function createStory() {
     " shirt." + " It was a " + adjective + 
     " day when we went to the " + place + "." +
     " Suddenly, a " + animal + " appeared and made me feel " + emotion + "." +
-    " Then we ate some " + food + " together. ";
+    " Then we ate " + number + " slices of " + food +
+    " while holding a " + object + ".";
 }
